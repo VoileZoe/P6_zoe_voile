be345727d2561106cf60063de128b3f0671d8a70
Zoé VOILE <zoevoile@gmail.com>
2021-12-08 15:44:38 +0100

    update user signup

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-router.post("/api/auth/signup", (req, res, next) => {
+router.post("/signup", (req, res, next) => {
   console.log(req.body);
   res.status(201).json({
     message: "Null add message",
   });
 });
 
-router.post("/api/auth/login", (req, res, next) => {
+router.post("/login", (req, res, next) => {
   console.log(req.body);
   res.status(201).json({
     message: "Null add message",
